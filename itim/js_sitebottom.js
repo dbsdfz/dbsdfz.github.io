dddae7565f0f88d9b2b0f5de7a510a3b8f50dbae
dbsdfz <106822322+dbsdfz@users.noreply.github.com>
2024-03-17 02:41:25 +0800

    Add files via upload

---
+++ itim/js_sitebottom.js
@@ -0,0 +1,5 @@
+document.writeln("<div class=\"container\">");
+document.writeln("<div class=\"row\">");
+document.writeln("<div class=\"pagebottombar\">© 2024 Impressive Teacher In Msannu (ITIM) Project</div>");
+document.writeln("</div>");
+document.writeln("</div>");
